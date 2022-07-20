101fa2ad842d6c9f71e7a1a6c10e35f1271e3e82
songjinhoon <jinhoon0319@gmail.com>
2022-07-20 23:09:30 +0900

    feat: EventControllerTest, EventController 기능추가

--- src/test/java/com/study/demoinflearnrestapi/domain/event/EventControllerTest.java
+++ src/test/java/com/study/demoinflearnrestapi/domain/event/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.study.demoinflearnrestapi.domain.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,12 +24,31 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("spring")
+                .description("spring rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 7, 2, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 7, 3, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2022, 7, 4, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2022, 7, 5, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 
 }
