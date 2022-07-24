e84fa07f42eb86f629bafa47192b1c3fc218d37c
songjinhoon <jinhoon0319@gmail.com>
2022-07-24 13:41:33 +0900

    test: Event Api BadRequest add

--- src/test/java/com/study/demoinflearnrestapi/domain/event/EventControllerTest.java
+++ src/test/java/com/study/demoinflearnrestapi/domain/event/EventControllerTest.java
@@ -37,7 +37,37 @@ public class EventControllerTest {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("spring")
+                .description("spring rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 7, 2, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 7, 3, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2022, 7, 4, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2022, 7, 5, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/event/")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100))) // 아이디 생성 규칙을 따라갈것이고
+                .andExpect(jsonPath("free").value(Matchers.not(true))) // boolean 의 기본 값인 false가 되겠지.
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
         Event event = Event.builder()
+                .id(100)
                 .name("spring")
                 .description("spring rest api")
                 .beginEnrollmentDateTime(LocalDateTime.of(2022, 7, 2, 14, 21))
@@ -58,13 +88,7 @@ public class EventControllerTest {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100))) // 아이디 생성 규칙을 따라갈것이고
-                .andExpect(jsonPath("free").value(Matchers.not(true))) // boolean 의 기본 값인 false가 되겠지.
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
     }
 
 }
