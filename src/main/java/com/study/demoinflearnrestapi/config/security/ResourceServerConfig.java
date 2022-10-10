9c374e579c46aa29f4c8dad30c7a5e3d70f9a89f
songjinhoon <jinhoon0319@gmail.com>
2022-10-10 21:46:16 +0900

    feat: 졸려

---
+++ src/main/java/com/study/demoinflearnrestapi/config/security/ResourceServerConfig.java
@@ -0,0 +1,34 @@
+package com.study.demoinflearnrestapi.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@EnableResourceServer
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("event");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .anonymous() //익명 사용자 허용
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").anonymous()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(new OAuth2AccessDeniedHandler())
+        ;
+    }
+
+}
