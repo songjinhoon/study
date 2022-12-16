e6c09718fb60957fc082c0ddb75447519d417327
songjinhoon <jinhoon0319@gmail.com>
2022-12-16 18:07:21 +0900

    fix: catalog-service data.sql modify

--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into catalog(catalog_id, name, stock, price) values ('CATALOG-001', 'marine', '100', '1000');
-insert into catalog(catalog_id, name, stock, price) values ('CATALOG-002', 'vulture', '110', '2000');
-insert into catalog(catalog_id, name, stock, price) values ('CATALOG-003', 'tank', '120', '3000');
+insert into tn_catalog(catalog_id, name, quantity, price) values ('CATALOG-001', 'marine', 100, 1000);
+insert into tn_catalog(catalog_id, name, quantity, price) values ('CATALOG-002', 'vulture', 110, 2000);
+insert into tn_catalog(catalog_id, name, quantity, price) values ('CATALOG-003', 'tank', 120, 3000);
