e775f969e1ee5fcf5a3aa328e4cd0445b69ad133
songjinhoon <jinhoon0319@gmail.com>
2022-12-26 15:15:32 +0900

    feat: promethus, grafana

--- user-service/src/main/java/com/study/spring/cloud/userservice/domain/controller/UserController.java
+++ user-service/src/main/java/com/study/spring/cloud/userservice/domain/controller/UserController.java
@@ -3,6 +3,7 @@ package com.study.spring.cloud.userservice.domain.controller;
 import com.study.spring.cloud.userservice.domain.dto.SignUpDto;
 import com.study.spring.cloud.userservice.domain.dto.UserDto;
 import com.study.spring.cloud.userservice.domain.service.UserService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ public class UserController {
     private final Environment environment;
 
     /***** CHECK *****/
+    @Timed(value = "user.status", longTask = true) // 이 메소드가 호출될 때, 메트릭스에 value값을 넣어준다. 결국 지표로 활용됨.
     @GetMapping("/health_check")
     public String status() {
         return String.format("It's Working in User Service"
@@ -32,6 +34,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "user.welcome", longTask = true)
     public String welcome() {
         return environment.getProperty("greeting.message");
     }
